d7f834e64228d2e35632a6d01f5736b6d901ab5f
Mohammad-Palla <mohammadpalla@yahoo.com>
2022-10-02 19:21:27 +0530

    Hello World in Solidity

---
+++ Solidity/helloworld.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+contract helloWorld {
+    constructor() public{
+
+    }
+
+    function print () public pure returns (string) {
+        return 'Hello World!';
+    }
+}
+
