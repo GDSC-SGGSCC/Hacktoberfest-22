a76bede6e179ed7fe8be363bedb4142d782031ed
Gokul_Zuzu <gokul.19ec@kct.ac.in>
2022-10-02 23:49:02 +0530

    Create password generator in golang

---
+++ Go/passwordgen.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "strings"
+    "time"
+)
+
+var (
+    lowerCharSet   = "abcdedfghijklmnopqrst"
+    upperCharSet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    specialCharSet = "!@#$%&*"
+    numberSet      = "0123456789"
+    allCharSet     = lowerCharSet + upperCharSet + specialCharSet + numberSet
+)
+
+func main() {
+    rand.Seed(time.Now().Unix())
+    minSpecialChar := 1
+    minNum := 1
+    minUpperCase := 1
+    passwordLength := 8
+    password := generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase)
+    fmt.Println(password)
+
+    minSpecialChar = 2
+    minNum = 2
+    minUpperCase = 2
+    passwordLength = 20
+    password = generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase)
+    fmt.Println(password)
+}
+
+func generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) string {
+    var password strings.Builder
+
+    //Set special character
+    for i := 0; i < minSpecialChar; i++ {
+        random := rand.Intn(len(specialCharSet))
+        password.WriteString(string(specialCharSet[random]))
+    }
+
+    //Set numeric
+    for i := 0; i < minNum; i++ {
+        random := rand.Intn(len(numberSet))
+        password.WriteString(string(numberSet[random]))
+    }
+
+    //Set uppercase
+    for i := 0; i < minUpperCase; i++ {
+        random := rand.Intn(len(upperCharSet))
+        password.WriteString(string(upperCharSet[random]))
+    }
+
+    remainingLength := passwordLength - minSpecialChar - minNum - minUpperCase
+    for i := 0; i < remainingLength; i++ {
+        random := rand.Intn(len(allCharSet))
+        password.WriteString(string(allCharSet[random]))
+    }
+    inRune := []rune(password.String())
+	rand.Shuffle(len(inRune), func(i, j int) {
+		inRune[i], inRune[j] = inRune[j], inRune[i]
+	})
+	return string(inRune)
+}
