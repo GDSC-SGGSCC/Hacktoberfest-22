e4ab432712bcb4aaf74b526eee61f0ece5e43fcb
Afnan Abdul Vasay <afnanabdulvasay234@gmail.com>
2022-10-04 12:57:09 +0530

    creating hello.m

---
+++ Objective-C/helloworld.m
@@ -0,0 +1,11 @@
+/* Hello world program in Objective-C */
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
