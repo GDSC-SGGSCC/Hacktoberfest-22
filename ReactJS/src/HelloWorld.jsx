419dfbefa9059b5ab5ae952307ff70b163bf4371
SaarthakTuli <saarthaktuli@gmail.com>
2022-10-07 19:33:51 +0530

    ReactJS BoilerPlate

---
+++ ReactJS/src/HelloWorld.jsx
@@ -0,0 +1,18 @@
+{/* 
+
+To run the script please run 
+npm start --prefix ./ReactJS
+
+So that in searches for the public and src folder inside the subfolder ReactJS
+
+*/}
+
+import React from 'react'
+
+const HelloWorld = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+};
+
+export default HelloWorld;
---
+++ ReactJS/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import HelloWorld from "./HelloWorld";
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
