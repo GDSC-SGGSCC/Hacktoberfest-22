8ac66d6f06c8839abdaff6b3da404df4961a4a61
AMAN SAINI <61909404+AMANSAINI7144@users.noreply.github.com>
2022-10-21 13:37:29 +0530

    Create myCodeForHelloWorld.cpp

---
+++ C++/myCodeForHelloWorld.cpp
@@ -0,0 +1,13 @@
+// C++ program to print Hello World
+// without using main() and semicolon
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define x main
+ 
+void x()
+{
+    if (cout << "Hello World") {
+    }
+}
